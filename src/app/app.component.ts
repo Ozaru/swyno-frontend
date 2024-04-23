ed9521b2e9306bc182b62a67396c83d96572aa6a
diego davel <diegodavel@gmail.com>
2024-04-22 23:19:36 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,71 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatRippleModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  
-  teste!: string
 
-  constructor(
-    private http: HttpClient,
-  ) {}
+  colunas: string[] = ["id", "nome", "acoes"]
+  movimentos: Movimento[] = []
 
   ngOnInit(): void {
-    this.http.get<string>(`${environment.apiUrl}`, { responseType: "text" as "json"}).subscribe(x => {
-      this.teste = x
+    this.movimentos = Array.from({length: 50}, (_, index) => {
+      return {
+        id: index + 1,
+        nome: `movimento ${index + 1}`
+      }
     })
   }
-  
+
+  inserir() {
+    const movimento = new Movimento()
+    let maiorId = 1
+    this.movimentos.forEach(x => {
+      maiorId = x.id > maiorId ? x.id : maiorId
+    })
+    movimento.id = maiorId + 1
+    const nome = prompt("Digite o nome:", `movimento ${movimento.id}`)
+    if(!nome) {
+      return
+    }
+    movimento.nome = nome
+    this.movimentos = [...this.movimentos, movimento]
+  }
+
+  editar(id: number) {
+    const movimento = this.movimentos.find(x => x.id == id)
+    if(!movimento) {
+      return
+    }
+    const nome = prompt("Digite o nome:", movimento?.nome)
+    if(!nome) {
+      return
+    }
+    movimento.nome = nome
+  }
+
+  deletar(id: number) {
+    this.movimentos = this.movimentos.filter(x => x.id != id)
+  }
+
+}
+
+class Movimento {
+  id!: number
+  nome!: string
 }
